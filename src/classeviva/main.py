26f08d7c6cf3c6b5653fadba6fa792a534f5f3db
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-05-15 21:11:03 +0200

    v0.0.2.dev8

--- src/classeviva/main.py
+++ src/classeviva/main.py
@@ -40,8 +40,8 @@ class Utente(object):
     async def accedi(self) -> None:
         intestazione = {
             "content-type": "application/json",
-            # "Z-Dev-ApiKey": "+zorro+",
-            # "User-Agent": "zorro/1.0"
+            "Z-Dev-ApiKey": "+zorro+",
+            "User-Agent": "zorro/1.0"
         }
         dati = {
             "ident": "null",
