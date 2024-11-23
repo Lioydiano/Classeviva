c907531a9b54999deb90a1702441963327fac380
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2024-11-23 17:40:45 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    "sphinx_rtd_theme"
+    "sphinx-rtd-theme"
 ]
 
 intersphinx_mapping = {
@@ -54,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
